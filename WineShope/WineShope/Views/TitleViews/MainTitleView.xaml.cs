806499eb4e99dfb020715ce3660f6a764c24b986
Kainã Fontoura Almeida <kainafa@hotmail.com>
2020-06-24 19:51:16 -0300

    Add project files.

---
+++ WineShope/WineShope/Views/TitleViews/MainTitleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WineShope.Views.TitleViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTitleView : ContentView
+    {
+        public MainTitleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
